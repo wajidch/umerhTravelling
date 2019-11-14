01db6a8a842e7a57bec7babd1e4580ddb60decf6
wajid <wajid.javeed@appflox.com>
2019-11-14 16:20:36 +0500

    build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { ComingSoonComponent } from './pages/coming-soon/coming-soon.component';
 
 const routes: Routes = [
 
-      { path:'',component:ComingSoonComponent,data: {title: ':: Al-Musawar | Travels &amp; Tours ::'}},
+      { path:'',component:ComingSoonComponent,data: {title: ':: Al-Musawar | Travels and Tours ::'}},
 
-      { path:'bookings',component:HomeComponent,data: {title: ':: Al-Musawar | Travels &amp; Tours ::'}},
+      { path:'bookings',component:HomeComponent,data: {title: ':: Al-Musawar | Travels and Tours ::'}},
      
   
-      {path:'**',redirectTo:'',pathMatch:'full',data: {title: ':: Al-Musawar | Travels &amp; Tours ::'}}
+      {path:'**',redirectTo:'',pathMatch:'full',data: {title: ':: Al-Musawar | Travels and Tours ::'}}
       
 ];
 
